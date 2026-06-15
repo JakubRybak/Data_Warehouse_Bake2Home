d5b9cc1807e6a9de36a9c4014d6a1e03228a5cb1
aryski <ryski.a@appotype.eu>
2026-06-15 10:58:34 +0200

    weather_fix

--- functions/collect_weather_to_parquet.py
+++ functions/collect_weather_to_parquet.py
@@ -1,20 +1,33 @@
-# main.py
 import functions_framework
+import pandas as pd
 import urllib.request, urllib.parse, json, ssl
 from google.cloud import bigquery
 
 
 @functions_framework.http
-def collect_weather(request):
+def collect_weather_to_parquet(request):
     client = bigquery.Client(project="bake2home-data-warehouse")
 
+    query_weather = "SELECT * FROM `bake2home-data-warehouse.bronze.weather`"
+
+    df_results_weather = client.query(query_weather).to_dataframe()
+
+    saved_weathers = {}
+
+    for _, row in df_results_weather.iterrows():
+        latitude = row["latitude"]
+        longitude = row["longitude"]
+        date = row["date"]
+        saved_weathers[(latitude, longitude, date)] = row
+
     query = """
-        SELECT sk_order_item, longitude, latitude, FORMAT_DATE('%Y-%m-%d', sk_date_order) as date_str FROM `gold.fact_order_item` fact_orders
+        SELECT longitude, latitude, FORMAT_DATE('%Y-%m-%d', sk_date_order) as date_str FROM `gold.fact_order_item` fact_orders
         LEFT JOIN `gold.dim_customer_address` dim_address ON dim_address.sk_customer_address = fact_orders.sk_customer_address
         WHERE weather_morning_was_rainy = -1 AND sk_date_order < CURRENT_DATE()
-        GROUP BY sk_order_item, longitude, latitude, date_str;
+        GROUP BY longitude, latitude, date_str;
     """
-    results = client.query(query).result()
+
+    missing_data = client.query(query).result()
 
     ctx = ssl.create_default_context()
     ctx.check_hostname = False
@@ -22,9 +35,11 @@ def collect_weather(request):
 
     def get_weather(dict):
         date = dict["date_str"]
+        latitude = dict["latitude"]
+        longitude = dict["longitude"]
         params = {
-            "latitude": dict["latitude"],
-            "longitude": dict["longitude"],
+            "latitude": latitude,
+            "longitude": longitude,
             "models": "best_match",
             "start_date": date,
             "end_date": date,
@@ -69,32 +84,23 @@ def collect_weather(request):
         else:
             weather_morning_was_rainy = -2
         return {
-            "weather_morning_precip_prob": weather_morning_precip_prob,
-            "weather_morning_precip_sum_mm": weather_morning_precip_sum_mm,
-            "weather_morning_was_rainy": weather_morning_was_rainy,
+            "latitude": [latitude],
+            "longitude": [longitude],
+            "date": [date],
+            "weather_morning_precip_prob": [weather_morning_precip_prob],
+            "weather_morning_precip_sum_mm": [weather_morning_precip_sum_mm],
+            "weather_morning_was_rainy": [weather_morning_was_rainy],
         }
 
-    rows = list(results)
-
     i = 0
-    for row in rows:
-        print(str(i))
-        res = get_weather(row)
-        query = (
-            "UPDATE `gold.fact_order_item` fact_orders "
-            + "SET weather_morning_was_rainy = "
-            + str(res["weather_morning_was_rainy"])
-            + ", "
-            + "weather_morning_precip_sum_mm = "
-            + str(res["weather_morning_precip_sum_mm"])
-            + ", "
-            + "weather_morning_precip_prob = "
-            + str(res["weather_morning_precip_prob"])
-            + " "
-            + "WHERE sk_order_item = "
-            + str(row["sk_order_item"])
+    for data in missing_data:
+        result_row = get_weather(data)
+        df_results_weather = pd.concat(
+            [df_results_weather, pd.DataFrame(result_row)], ignore_index=True
         )
-        res = client.query(query).result()
+        if i % 10 == 0:
+            df_results_weather.to_parquet(
+                "gs://bake2home-raw-data/weather/weather.parquet"
+            )
         i = i + 1
-
-    return 200
+    df_results_weather.to_parquet("gs://bake2home-raw-data/weather/weather.parquet")
